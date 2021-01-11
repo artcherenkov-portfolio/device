3ea8a757288554735088d54209f482cc80016bcc
Artem Cherenkov <art.cherenkov@gmail.com>
2021-01-11 12:56:28 +0700

    minor changes

--- gulpfile.js
+++ gulpfile.js
@@ -101,7 +101,7 @@ gulp.task("build", gulp.series(
 
 gulp.task("start", gulp.series("build", "server"));
 
-var ghPages = require('gh-pages');
+const ghPages = require('gh-pages');
 
 gulp.task('deploy', function() {
   ghPages.publish('./public', function(err) {});
